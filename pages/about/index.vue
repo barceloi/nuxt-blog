ed61650348e1f61b11c7622a77c7e736bca89089
Ignacio Barcelo <ignacio.barcelo@poliglota.org>
2021-10-08 18:50:30 -0300

    first commit

---
+++ pages/about/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about-page">
+    <h1>Sobre mi</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus tempora
+      et quo qui, obcaecati ratione asperiores sit numquam maxime voluptate
+      beatae iure, accusamus a corrupti eaque hic incidunt voluptatem enim.
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.about-page {
+  text-align: center;
+  padding: 30px;
+}
+</style>
